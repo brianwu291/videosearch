ee6c65e14e8601a716dae5f9dec231ad8aff3004
brianwu <38502299+brianwu291@users.noreply.github.com>
2018-11-25 02:36:57 +0800

    Updates

---
+++ precache-manifest.3f5bc0d351bbe30274742a2d59519189.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fbd45bd724fc7cdefbd2",
+    "url": "/videosearch/static/js/runtime~main.fbd45bd7.js"
+  },
+  {
+    "revision": "51a794cdc42c1959bb18",
+    "url": "/videosearch/static/js/main.51a794cd.chunk.js"
+  },
+  {
+    "revision": "08d9b7f390a9a00f0bdb",
+    "url": "/videosearch/static/js/1.08d9b7f3.chunk.js"
+  },
+  {
+    "revision": "51a794cdc42c1959bb18",
+    "url": "/videosearch/static/css/main.69cf69c7.chunk.css"
+  },
+  {
+    "revision": "2f26b21fe562d3f4c55506ea4fcd5aa3",
+    "url": "/videosearch/index.html"
+  }
+];
